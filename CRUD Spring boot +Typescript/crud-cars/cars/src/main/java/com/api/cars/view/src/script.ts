c15e69f14f3342aa3ffd95e9f17fc136210c1eb7
André Matos <andrermatos90@gmail.com>
2023-11-04 15:33:16 -0300

    ...

--- CRUD Spring boot +Typescript/crud-cars/cars/src/main/java/com/api/cars/view/src/script.ts
+++ CRUD Spring boot +Typescript/crud-cars/cars/src/main/java/com/api/cars/view/src/script.ts
@@ -1,20 +1,44 @@
-
-
-
 // URL da API que você deseja acessar
 const apiUrl = 'http://localhost:8080/cars';
 
 // Função para fazer a requisição GET
 async function carregaCarros() {
-    let container:any = document.querySelector('.container');
-
   try {
     const response = await fetch(apiUrl);
 
     // Verifica se a resposta foi bem-sucedida (código de status 200)
     if (response.ok) {
-      const data = await response.json();
-      console.log('Dados da resposta:', data);
+      const database: any[] = await response.json();
+
+      console.log('Dados da resposta:', database);
+      const container:any = document.querySelector('.container');
+      
+      // Crie a tabela com cabeçalhos das colunas
+      const tablehtml = `
+        <table>
+          <tr>
+            <th>Modelo</th>
+            <th>Montadora</th>
+            <th>Ano</th>
+            <th>Tipo de Combustível</th>
+            <th>0 a 100km/h</th>
+            <th>Importado</th>
+          </tr>
+          ${database.map(carro=> `
+            <tr>
+              <td>${carro.modelCar}</td>
+              <td>${carro.brandCar}</td>
+              <td>${carro.modelYear}</td>
+              <td>${carro.fuelType}</td>
+              <td>${carro.zeroToHundred}</td>
+              <td>${carro.imported}</td>
+            </tr>
+          `).join('')}
+        </table>`;
+      
+      // Limpa o conteúdo anterior do container
+      container.innerHTML = '';
+      container.innerHTML = tablehtml;
     } else {
       console.error('A requisição falhou com código de status:', response.status);
     }
@@ -22,5 +46,4 @@ async function carregaCarros() {
     console.error('Ocorreu um erro ao fazer a requisição:', error);
   }
 }
-
 
