bc7bda4e9373959de506c97da552ef7c916d5213
André Matos <andrermatos90@gmail.com>
2023-12-09 16:35:17 -0300

    ..

--- Aplicação Consumo API de Noticias/Noticias/src/app/components/news/news.component.ts
+++ Aplicação Consumo API de Noticias/Noticias/src/app/components/news/news.component.ts
@@ -1,28 +1,25 @@
-import { News } from './../../interfaces/News';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { News } from 'src/app/interfaces/News';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  news: News[] = []; // Altere o tipo conforme necessário
 
-  news:News[]=[]
+  constructor(private newsService: NewsService) {}
 
-  constructor(private newsService:NewsService){
-    this.getNews()
+  ngOnInit(): void {
+    this.getNews();
   }
 
-
-  getNews():void{
+  getNews(): void {
     this.newsService.getAll().subscribe((response) => {
       this.news = response;
       console.log(this.news);
-
     });
-
   }
-
 }
