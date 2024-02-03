03eb49f591e3c42997433e53af1a9f1d33c32c27
André Matos <andrermatos90@gmail.com>
2024-02-03 15:10:36 -0300

    inicio

---
+++ Projeto GameList/GameList/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
