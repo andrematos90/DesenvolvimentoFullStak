52827205865b23eca55d29ea56c756fc5b739175
André Matos <andrermatos90@gmail.com>
2024-02-07 14:02:08 -0300

    header, footer, pages

--- Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/app.module.ts
+++ Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
