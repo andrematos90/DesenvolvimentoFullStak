9afac5ca74315c01c5feb2bce392da679f0ccf26
André Matos <andrermatos90@gmail.com>
2024-02-09 15:38:21 -0300

    css pagina de games

--- Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/services/games.service.ts
+++ Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/services/games.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Response } from '../interfaces/Response';
 
 @Injectable({
   providedIn: 'root'
