a8598362bdb2f429d88b08bf26be0811e0835776
André Matos <andrermatos90@gmail.com>
2024-02-14 09:07:49 -0300

    pagina de categorias ok

--- Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/components/pages/categories/platform/platform.component.ts
+++ Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/components/pages/categories/platform/platform.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Game } from 'src/app/interfaces/Game';
+import { GamesService } from 'src/app/services/games.service';
+import { environment } from 'src/environments/environment';
+import { EventService } from 'src/app/services/event.service';
+import { StateService } from 'src/app/services/state.service';
+import { DetailsService } from 'src/app/services/details.service';
 
 @Component({
   selector: 'app-platform',
@@ -7,10 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatformComponent implements OnInit {
 
-  constructor() { }
+  baseApiUrl = environment.baseApiUrl;
+  allGames: Game[] = [];
+  show: boolean = false;
+  ok: string = 'Role-playing (RPG), Shooter';
+
+
+
+  constructor(
+    private gameService: GamesService,
+    private eventService: EventService,
+    private stateService: StateService,
+    private detailService: DetailsService,
+  ) { }
 
   ngOnInit(): void {
+    this.gameService.getGames().subscribe(response => {
+      this.allGames = response
+
+
+    });
+
+    this.stateService.showModal$.subscribe((value) => {
+      this.show = value;
+    });
+
   }
+
+  openModal(game: Game) {
+    this.eventService.emitToggleEvent();
+    this.detailService.setDetailsGame(game);
+  }
+
 
 }
 
--- Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/components/pages/categories/role-playng-adventure/role-playng-adventure.component.ts
+++ Projeto GameList - JavaSpringBoot + Angular/front-end/GameList/src/app/components/pages/categories/role-playng-adventure/role-playng-adventure.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Game } from 'src/app/interfaces/Game';
+import { GamesService } from 'src/app/services/games.service';
+import { environment } from 'src/environments/environment';
+import { EventService } from 'src/app/services/event.service';
+import { StateService } from 'src/app/services/state.service';
+import { DetailsService } from 'src/app/services/details.service';
 
 @Component({
   selector: 'app-role-playng-adventure',
@@ -7,9 +14,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RolePlayngAdventureComponent implements OnInit {
 
-  constructor() { }
+  baseApiUrl = environment.baseApiUrl;
+  allGames: Game[] = [];
+  show: boolean = false;
+  ok: string = 'Role-playing (RPG), Shooter';
+
+
+
+  constructor(
+    private gameService: GamesService,
+    private eventService: EventService,
+    private stateService: StateService,
+    private detailService: DetailsService,
+  ) { }
 
   ngOnInit(): void {
+    this.gameService.getGames().subscribe(response => {
+      this.allGames = response
+
+
+    });
+
+    this.stateService.showModal$.subscribe((value) => {
+      this.show = value;
+    });
+
   }
+
+  openModal(game: Game) {
+    this.eventService.emitToggleEvent();
+    this.detailService.setDetailsGame(game);
+  }
+
 
 }
