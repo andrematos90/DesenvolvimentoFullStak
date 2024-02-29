25ff23a0ffb17c9c2f38bd335bb148a7d23e462f
André Matos <andrermatos90@gmail.com>
2024-02-29 11:12:24 -0300

    header, componente de iconer e footer

--- ProjetoToDoList - JavaSpringBoot + Angular/fron-end/ToDolist/src/app/app.module.ts
+++ ProjetoToDoList - JavaSpringBoot + Angular/fron-end/ToDolist/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { IconModule } from './icon/icon.module';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    IconModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
