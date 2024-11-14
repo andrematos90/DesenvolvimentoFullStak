c9ed167f9a21e52b869968c2090b16914c624051
André Matos <andrerosadematos90@gmail.com>
2024-11-14 09:29:21 -0300

    ..

--- Projeto Moments - Angular + Adonis/Moments/src/app/components/moment-form/moment-form.component.ts
+++ Projeto Moments - Angular + Adonis/Moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 
 
@@ -7,8 +9,37 @@ import { Component, Input } from '@angular/core';
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit{
 
- @Input() btnText!: String
+ @Input() btnText!: String;
+
+ momentForm!: FormGroup;
+
+ constructor(){}
+
+ ngOnInit(): void {
+     this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+     });
+ }
+
+ get title(){
+  return this.momentForm.get('title');
+ }
+
+ get description(){
+  return this.momentForm.get('description');
+ }
+
+ get image(){
+  return this.momentForm.get('image');
+ }
+
+ submit(){
+  console.log("enviou formulário")
+ }
 
 }
