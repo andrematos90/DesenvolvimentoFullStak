dbe6121c4dcef3c3e3ed950ad9075271c138f629
André Matos <andrerosadematos90@gmail.com>
2025-01-27 14:22:56 -0300

    entidade GAME

---
+++ Projeto GameList - Angular + JavaSpringBoot/back end/gamelist/src/main/java/com/estudos/gamelist/entities/Game.java
@@ -0,0 +1,142 @@
+package com.estudos.gamelist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+//notation que configura a classe para que ela seja equivalente a uma tabela do banco
+@Entity
+//define o nome da tabela
+@Table(name = "Tb_game")
+public class Game {
+
+    //define o id como chave primaria
+    @Id
+    //autoincremeta o id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    //altera o nome da coluna
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    //construtor sem parametros
+    public Game(){
+
+    }
+
+    //construtor com parametros
+    public Game(
+            Long id,
+            String title,
+            Integer year,
+            String genre,
+            String platforms,
+            Double score,
+            String imgUrl,
+            String shortDescription,
+            String longDescription
+    ){
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    //métodos getter e setters
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return  title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public Integer getYear(){
+        return year;
+    }
+
+    public void setYear(Integer year){
+        this.year = year;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+    public String getPlatforms(){
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms){
+        this.platforms = platforms;
+    }
+
+    public Double getScore(){
+        return score;
+    }
+
+    public void setScore(Double score){
+        this.score = score;
+    }
+
+    public String getImgUrl(){
+        return  imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl){
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription(){
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription){
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription(){
+        return  longDescription;
+    }
+
+    public void setLongDescription(String longDescription){
+        this.longDescription = longDescription;
+    }
+
+    //métodos para comparar games
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Game game)) return false;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
